221a26e8293feaa0cddb3330dda6f8335db6e3f7
aravindbhaskar41 <aravindbhaskar41@gmail.com>
2020-10-18 22:45:11 +0530

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,19 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-           <>
-//write your code here
-	</>
-        )
-    }
+  render() {
+    const relativesList = ["Batman", "Superman", "Flash", "WonderWoman"];
+    return (
+      <>
+        <ol key="relativesList">
+          {relativesList.map((el, index) => (
+            <li key={`relativeListItem${index}`}>{el}</li>
+          ))}
+        </ol>
+      </>
+    );
+  }
 }
-
 
 export default App;
